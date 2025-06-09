15d0484b24cb17289cce26df03e215751e31819e
sm4441 <samuelribeirodossantossilva@gmail.com>
2025-06-09 14:40:09 -0300

    Add files via upload

---
+++ src/DAO/cliente/addProduto.js
@@ -0,0 +1,20 @@
+const { conexao } = require('../conexao.js')
+
+async function inserirProduto(codigo, nome, id_categoria, preco) {
+    const sql = `
+        INSERT INTO tbl_produtos (codigo, nome, id_categoria, preco)
+        VALUES (?, ?, ?, ?);
+    `;
+
+    const conn = await conexao();
+    try {
+        const [resultado] = await conn.query(sql, [codigo, nome, id_categoria, preco]);
+        return { sucesso: true, idInserido: resultado.insertId };
+    } catch (err) {
+        return { sucesso: false, erro: err.message };
+    } finally {
+        await conn.end();
+    }
+}
+
+module.exports = { inserirProduto }; 
