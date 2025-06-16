c692998e471816f47f2d77a0753531f626d365d6
sm4441 <samuelribeirodossantossilva@gmail.com>
2025-06-16 14:39:56 -0300

    Update addPedido.js

--- src/DAO/cliente/addPedido.js
+++ src/DAO/cliente/addPedido.js
@@ -1,12 +1,13 @@
 const { conexao } = require('../conexao.js')
 
-async function inserirPedido(numero, data_elaboracao, cliente) {
+async function inserirPedido(numero, data_elaboracao, cliente_id) {
     const sql = `
-INSERT INTO tbl_pedido (numero, data_elaboracao, cliente_id) VALUES`;
+INSERT INTO tbl_pedido (numero, data_elaboracao, cliente_id) VALUES (?, ?, ?);
+`;
 
     const conn = await conexao();
     try {
-        const [resultado] = await conn.query(sql, [numero, data_elaboracao, cliente]);
+        const [resultado] = await conn.query(sql, [numero, data_elaboracao, cliente_id]);
         await conn.end();
         return { sucesso: true, idInserido: resultado.insertId };
     } catch (err) {
